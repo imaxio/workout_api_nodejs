e417ea8f309e9c99ce1c57a22a1d2840fded5aaa
IMaxio <smaxgess52@gmail.com>
2024-10-09 20:08:16 +0700

    v4-db

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require("sequelize");
 
 // For PostgreSQL or MySQL:
-const sequelize = new Sequelize("your_database", "username", "password", {
-    host: "localhost",
+const sequelize = new Sequelize("workout_api_db", "workout", "MkyxU5Ww6rh1Nguzciso9iwaZ0M4ozC0", {
+    host: "dpg-cs37use8ii6s738i2ns0-a",
     dialect: "postgres", // or 'mysql'
 });
 
