155a5b4bac828df3222073fba9ed09f412a42129
IMaxio <smaxgess52@gmail.com>
2024-10-10 18:01:05 +0700

    v10

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const goalController = require('../controllers/goalController');
 const router = express.Router();
 
-router.post('/users/:user_id/', goalController.createGoal); // Create a new goal
-router.get('/users/:user_id/', goalController.getUserGoals); // Get a goals user
+router.post('/users/:user_id/goals', goalController.createGoal); // Create a new goal
+router.get('/users/:user_id/goals', goalController.getUserGoals); // Get a goals user
 router.get('/', goalController.getAllGoals); // Get all goals
 router.get('/:id', goalController.getGoalById); // Get goal by ID
 router.put('/:id', goalController.updateGoal); // Update goal by ID
