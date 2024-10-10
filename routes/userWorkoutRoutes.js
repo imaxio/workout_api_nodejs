f3955a9460b0ee97c9f6c8bb70fafdc8114b393d
IMaxio <smaxgess52@gmail.com>
2024-10-10 17:37:17 +0700

    v9

--- routes/userWorkoutRoutes.js
+++ routes/userWorkoutRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const userWorkoutController = require('../controllers/userWorkoutController');
 const router = express.Router();
 
-router.post('/user/:user_id/', userWorkoutController.createUserWorkout); // Create a new user workout
-router.get('/user/:user_id/', userWorkoutController.getAllUserWorkouts); // Get A user workouts
+router.post('/users/:user_id/', userWorkoutController.createUserWorkout); // Create a new user workout
+router.get('/users/:user_id/', userWorkoutController.getAllUserWorkouts); // Get A user workouts
 router.get('/', userWorkoutController.getAllUserWorkouts); // Get all user workouts
 router.get('/:id', userWorkoutController.getUserWorkoutById); // Get user workout by ID
 router.put('/:id', userWorkoutController.updateUserWorkout); // Update user workout by ID
