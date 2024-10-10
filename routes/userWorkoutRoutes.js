a38e201a527bf99d880a8abc8bdc94b25fb01ae3
IMaxio <smaxgess52@gmail.com>
2024-10-10 20:18:44 +0700

    v13

--- routes/userWorkoutRoutes.js
+++ routes/userWorkoutRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 router.post('/users/:user_id/user-workouts', userWorkoutController.createUserWorkout); // Create a new user workout
 router.get('/users/:user_id/user-workouts', userWorkoutController.getAllUserWorkouts); // Get A user workouts
-router.get('user-workouts/', userWorkoutController.getAllUserWorkouts); // Get all user workouts
-router.get('user-workouts/:id', userWorkoutController.getUserWorkoutById); // Get user workout by ID
-router.put('user-workouts/:id', userWorkoutController.updateUserWorkout); // Update user workout by ID
-router.delete('user-workouts/:id', userWorkoutController.deleteUserWorkout); // Delete user workout by ID
+router.get('/user-workouts', userWorkoutController.getAllUserWorkouts); // Get all user workouts
+router.get('/user-workouts/:id', userWorkoutController.getUserWorkoutById); // Get user workout by ID
+router.put('/user-workouts/:id', userWorkoutController.updateUserWorkout); // Update user workout by ID
+router.delete('/user-workouts/:id', userWorkoutController.deleteUserWorkout); // Delete user workout by ID
 
 module.exports = router;
--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const workoutController = require('../controllers/workoutController');
 const router = express.Router();
 
-router.post('workout/', workoutController.createWorkout); // Create a new workout
-router.get('workout/', workoutController.getAllWorkouts); // Get all workouts
-router.get('workout/:id', workoutController.getWorkoutById); // Get workout by ID
-router.put('workout/:id', workoutController.updateWorkout); // Update workout by ID
-router.delete('workout/:id', workoutController.deleteWorkout); // Delete workout by ID
+router.post('/workout', workoutController.createWorkout); // Create a new workout
+router.get('/workout', workoutController.getAllWorkouts); // Get all workouts
+router.get('/workout/:id', workoutController.getWorkoutById); // Get workout by ID
+router.put('/workout/:id', workoutController.updateWorkout); // Update workout by ID
+router.delete('/workout/:id', workoutController.deleteWorkout); // Delete workout by ID
 
 module.exports = router;
